701860662b0879c161eabd950d5c2370e49fec78
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 19:05:23 +0800

    pseudocode

---
+++ sum_of_the_previous_and_current_number.py
@@ -0,0 +1,12 @@
+# Exercise 2: Print the sum of the current number and the previous number
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+ 
+# pseudocode
+
+# create a loop 
+
+# add the current and previous number
+
+# print the list of current number, previous number, and their sum
+
+# change the value of previous number to current number
